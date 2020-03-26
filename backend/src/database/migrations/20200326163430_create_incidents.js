0f5092c018c35ea5c758c6f74efdd3f3b618f9d6
Hans Almeida <hansalmeida@hotmail.com>
2020-03-26 18:59:43 -0300

    fix on migration with wrong table name

--- backend/src/database/migrations/20200326163430_create_incidents.js
+++ backend/src/database/migrations/20200326163430_create_incidents.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("incidents", table => {
     table.increments()
-    table.string("name").notNullable()
+    table.string("title").notNullable()
     table.string("description").notNullable()
     table.decimal("value").notNullable()
 
