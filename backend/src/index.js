33bcdf1ca8d063bbeceeee463649aa165b667100
Hans Almeida <hansalmeida@hotmail.com>
2020-03-26 17:09:28 -0300

    Enabled json middlware in express

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ const routes = require("./routes.js")
 
 const app = express()
 
+app.use(express.json())
+
 app.use(routes)
 
+console.clear() // Only for clearing the nodemon console while in development mode
 app.listen(3333)
