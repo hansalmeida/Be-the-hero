f7d271b0e48d771154a66f0341dae3a6dd1b64e1
Hans Almeida <hansalmeida@hotmail.com>
2020-03-27 17:30:58 -0300

    Update Detail Page created: Layout and Style Go back arrow logic.

--- mobile/src/pages/Detail/index.jsx
+++ mobile/src/pages/Detail/index.jsx
@@ -1,10 +1,53 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity, Image } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native"
+import styles from "./styles"
+
+import logoImg from "../../assets/logo.png"
 
 const Detail = () => {
+  const navigation = useNavigation()
+
+  function navigateBack() {
+    navigation.goBack()
+  }
+
   return (
-    <View>
-      <Text>Detail Component</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#e82041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>ONG name 1</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>Caso name 1</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>E-Mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
