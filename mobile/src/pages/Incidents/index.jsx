c9ec39a45de7e115e73926d3a2f439a66952b31a
Hans Almeida <hansalmeida@hotmail.com>
2020-03-27 16:29:57 -0300

    Created Routes setup and Pages list: Routes Navigator. Incidents component. Detail component.

---
+++ mobile/src/pages/Incidents/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const Incidents = () => {
+  return (
+    <View>
+      <Text>Incidents Component</Text>
+    </View>
+  )
+}
+
+export default Incidents
