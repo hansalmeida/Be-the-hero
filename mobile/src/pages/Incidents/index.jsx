1d2e368c598028d449dccb767be64f22273a769c
Hans Almeida <hansalmeida@hotmail.com>
2020-03-27 17:04:59 -0300

    Update Incidents Component Layout and Styles complete, but still logicless.

--- mobile/src/pages/Incidents/index.jsx
+++ mobile/src/pages/Incidents/index.jsx
@@ -1,10 +1,49 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Image } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import styles from "./styles"
+
+import logoImg from "../../assets/logo.png"
+import { TouchableOpacity, FlatList } from "react-native-gesture-handler"
 
 const Incidents = () => {
   return (
-    <View>
-      <Text>Incidents Component</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia.
+      </Text>
+
+      <FlatList
+        style={styles.incidentList}
+        data={[1, 2]}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>ONG name 1</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Caso name 1</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   )
 }
