9ee4b799fb6ef389b5ab72fb90bf466190b35ace
Hans Almeida <hansalmeida@hotmail.com>
2020-03-26 23:21:54 -0300

    Refactor React router dom Link added for SPA instead of reloding all pages

--- frontend/src/pages/Logon/index.jsx
+++ frontend/src/pages/Logon/index.jsx
@@ -4,6 +4,7 @@ import "./styles.scss"
 import heroesImg from "../../assets/heroes.png"
 import logoImg from "../../assets/logo.svg"
 import { FiLogIn } from "react-icons/fi"
+import { Link } from "react-router-dom"
 
 const Logon = props => {
   return (
@@ -14,10 +15,10 @@ const Logon = props => {
           <h1>Faça seu logon</h1>
           <input placeholder="Sua ID" />
           <button className="button">Entrar</button>
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
