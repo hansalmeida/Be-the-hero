afddd7bb1e2d367bc18030c0228f30cb1b3617d9
Hans Almeida <hansalmeida@hotmail.com>
2020-03-28 22:07:22 -0300

    Update Routes created: Validation added with celebrate package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const routes = require("./routes.js")
 const cors = require("cors")
+const { errors } = require("celebrate")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
 app.use(routes)
+app.use(errors())
 
 console.clear() // Only for clearing the nodemon console while in development mode
 app.listen(3333)
